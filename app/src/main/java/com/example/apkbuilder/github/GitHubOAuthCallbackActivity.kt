90a4c8e6b081681a0f242af972ddd6acd3f5d818
iposoasys-dotcom <iposoasys@gmail.com>
2026-08-24 12:38:44 +0530

    Fix GitHub OAuth coroutine launch import

--- app/src/main/java/com/example/apkbuilder/github/GitHubOAuthCallbackActivity.kt
+++ app/src/main/java/com/example/apkbuilder/github/GitHubOAuthCallbackActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class GitHubOAuthCallbackActivity : Activity() {
 
